939daf8b241bfbdc6468fe0425bd7ff88bf1cd05
lanmower <almagestfraternite@gmail.com>
2022-02-25 11:57:24 +0200

    Delete stdserver.js

--- stdserver.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-if(process.argv.length == 3) {
-  const key = process.argv[process.argv.length-1];
-  const pubKey = require('./relay.js')().serve(key, 0, true)
-} else console.log("usage: hyper-relay-server <key>");
-
- 
